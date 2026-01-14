da61746067602e3ecf43b8fbd2fad45a6d47300b
lmoma <luchasmm@gmail.com>
2026-01-14 12:49:30 -0500

    changed config for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/iyf', // Ensures relative paths for GitHub Pages
+    base: '/', // Serve site at custom-domain root (changed from '/iyf')
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
